3c7385de866aa404aa75acbfce00534f5a8291bf
ltran96 <dontr710@gmail.com>
2025-09-01 22:58:59 -0700

    print some results for now

--- MediaPipe_Tutorial/mediapipe_example.py
+++ MediaPipe_Tutorial/mediapipe_example.py
@@ -9,15 +9,32 @@ def main():
     mp_hands = mp.solutions.hands
 
     # Setting up a web cam
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        ret, frame = cap.read()
+    cap = cv2.VideoCapture(0) # This works for MacOS too.
 
-        cv2.imshow("Hand Tracking", frame)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+    with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            # Detection (convert openCV's BGR to RGB so that MediaPipe can process it)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Set flags
+            image.flags.writeable = False
+
+            # Make Detection
+            returned = hands.process(image)
+            image.flags.writeable = True
+
+            # Convert back to BGR for OpenCV
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            print(returned) # Print detection results
+            
+            cv2.imshow("Hand Tracking", frame)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+            
+        cap.release()
+        cv2.destroyAllWindows()
 
 # standard Python entry point
 if __name__ == "__main__":
